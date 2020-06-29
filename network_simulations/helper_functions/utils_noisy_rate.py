ba99d180b7b180b7fcef0277f2811ace8b7c71c1
Gaspar Cano <gaspar.fcano@gmail.com>
2020-06-29 14:32:50 +0200

    calculate correlations with peak amplitudes

--- network_simulations/helper_functions/utils_noisy_rate.py
+++ network_simulations/helper_functions/utils_noisy_rate.py
@@ -31,11 +31,15 @@ def get_peak_data(t, b, b_pulses, sim_type):
 
     peaks = detect_peaks(lowpass_b, mph=pm.b_findpeak_height, mpd=int(pm.b_findpeak_dist/dt), show=False)
 
+    print('Found %d peaks'%len(peaks))
+
     # Find start and end points:
     start = np.zeros(peaks.size,dtype=int)
     end = np.zeros(peaks.size,dtype=int)
     duration = np.zeros(peaks.size)
+    amplitudes = np.zeros(peaks.size)
     if peaks.size > 0:
+        amplitudes = lowpass_b[peaks]
         IEI = np.zeros(peaks.size-1, dtype=float)
 
         i = 0
@@ -44,17 +48,17 @@ def get_peak_data(t, b, b_pulses, sim_type):
         halfmax = lowpass_b[peaks[0]]/2
         while(i < t.size and j < peaks.size):
 
-            if (in_peak == False) and (lowpass_b[i] > halfmax) and (i < peaks[j]):
+            if (in_peak == False) and (lowpass_b[i] >= halfmax) and (i < peaks[j]):
                 # print('passed start')
                 start[j] = i
                 in_peak = True
 
-            if (in_peak == True) and (lowpass_b[i] < halfmax) and (i > peaks[j]):
+            if (in_peak == True) and (lowpass_b[i] <= halfmax) and (i > peaks[j]):
                 # print('passed end')
                 end[j] = i
                 in_peak = False
 
-            if (j < peaks.size - 1) and (i > peaks[j]) and (peaks[j+1] - i < i - peaks[j]):
+            if (j < peaks.size - 1) and (in_peak == False) and (i > peaks[j]) and (peaks[j+1] - i < i - peaks[j]):
                 j = j + 1
                 halfmax = lowpass_b[peaks[j]]/2
 
@@ -62,14 +66,29 @@ def get_peak_data(t, b, b_pulses, sim_type):
 
         duration = t[end] - t[start]
 
+        for i in range(len(peaks)):
+            if lowpass_b[peaks[i]] < 45:
+                t_peak_err = t[peaks[i]]
+                print('error in peak %d at time %d'%(i,t_peak_err))
+                plt.plot(t,lowpass_b)
+                plt.xlim([t_peak_err - 500, t_peak_err + 500])
+                plt.axvline(t[peaks[i]])
+                plt.axvline(t[peaks[i-1]])
+                plt.axvline(t[peaks[i+1]])
+                plt.axvline(t[start[i]], color='black')
+                plt.axvline(t[end[i]], color='red')
+                plt.axhline(lowpass_b[peaks[i]]/2,ls='--')
+                plt.show()
         if (duration <= 0).any():
-            print('ERROR: Failed finding peaks')
+            print('ERROR1: Failed finding peaks')
             return False
 
         # Calculate Inter-Event-Intervals:
         for i in range(peaks.size-1):
             IEI[i] = (t[start[i+1]] - t[end[i]])*1e-3 # convert to seconds
 
+        amp_prev = 0
+        amp_next = 0
         duration_prev = 0
         duration_next = 0
         IEI_prev = 0
@@ -78,6 +97,8 @@ def get_peak_data(t, b, b_pulses, sim_type):
         b_pulses_success = 0
 
         if (sim_type is 'spont'):
+            amp_prev = amplitudes[1:]
+            amp_next = amplitudes[:-1]
             duration_prev = duration[1:]
             duration_next = duration[:-1]
             IEI_prev = IEI
@@ -96,7 +117,7 @@ def get_peak_data(t, b, b_pulses, sim_type):
                 evoked_peaks = np.array([-1])
                 for i in range(len(b_pulses_onset)):
                     for j in range(peaks.size):
-                        if (t[start[j]] - b_pulses_onset[i]) > 0 and (t[start[j]] - b_pulses_onset[i]) <= 50:
+                        if (t[peaks[j]] - b_pulses_onset[i]) > 0 and (t[peaks[j]] - b_pulses_onset[i]) <= 50:
                             b_pulses_success[i] = 1
                             evoked_peaks = np.append(evoked_peaks,j)
                 if len(evoked_peaks) > 1:
@@ -107,6 +128,8 @@ def get_peak_data(t, b, b_pulses, sim_type):
                     # Get data only from evoked peaks:
                     start = start[evoked_peaks]
                     end = end[evoked_peaks]
+                    amp_prev = amplitudes[evoked_peaks]
+                    amp_next = amplitudes[evoked_peaks]
                     duration_prev = duration[evoked_peaks]
                     duration_next = duration[evoked_peaks]
                     IEI_prev = IEI[evoked_peaks-1]
@@ -115,6 +138,8 @@ def get_peak_data(t, b, b_pulses, sim_type):
                     evoked_peaks = 0
                     start = 0
                     end = 0
+                    amp_prev = 0
+                    amp_next = 0
                     duration_prev = 0
                     duration_next = 0
                     IEI_prev = 0
@@ -122,17 +147,18 @@ def get_peak_data(t, b, b_pulses, sim_type):
             else:
                 peak_data = False
 
-        peak_data = start, end, duration_prev, duration_next, IEI_prev, IEI_next
+        peak_data = start, end, amp_prev, amp_next, duration_prev, duration_next, IEI_prev, IEI_next
+
     if peaks.size == 0:
-        print('ERROR: Failed finding peaks')
+        print('ERROR2: Failed finding peaks')
         peak_data = False
         b_pulses_onset = 0
         b_pulses_success = 0
 
     fit_params = np.array([0,0,0])
     if peak_data != False:
-        peak_start, peak_end, peaks_duration_prev, peaks_duration_next, peaks_IEI_prev, peaks_IEI_next = peak_data
-        try: fit_params, _ = curve_fit(fit_func, peaks_IEI_prev, peaks_duration_prev,p0=(2,2,68), bounds=(0,[100, 100, 100]))
+        peak_start, peak_end, peaks_amp_prev, peaks_amp_next, peaks_duration_prev, peaks_duration_next, peaks_IEI_prev, peaks_IEI_next = peak_data
+        try: fit_params, _ = curve_fit(fit_func, peaks_IEI_prev, peaks_amp_prev,p0=(2,2,68), bounds=(0,[100, 100, 100]))
         except: None
 
     if peak_data != False:
@@ -144,16 +170,20 @@ def get_peak_data(t, b, b_pulses, sim_type):
         elif sim_type == 'evoke':
             print('IEI = (%.2lf +/- %.2lf) s'%(np.mean(np.concatenate((peaks_IEI_prev,peaks_IEI_next))),np.std(np.concatenate((peaks_IEI_prev,peaks_IEI_next)))))
         c, p = pearsonr(peaks_IEI_prev,peaks_duration_prev)
-        print('Correlation with Previous IEI: c = %.3lf, p = %.2e'%(c,p))
+        print('FWHM correlation with Previous IEI: c = %.3lf, p = %.2e'%(c,p))
         c, p = pearsonr(peaks_IEI_next,peaks_duration_next)
-        print('Correlation with Next IEI: c = %.3lf, p = %.2e'%(c,p))
+        print('FWHM correlation with Next IEI: c = %.3lf, p = %.2e'%(c,p))
+        c, p = pearsonr(peaks_IEI_prev,peaks_amp_prev)
+        print('Amplitude correlation with Previous IEI: c = %.3lf, p = %.2e'%(c,p))
+        c, p = pearsonr(peaks_IEI_next,peaks_amp_next)
+        print('Amplitude correlation with Next IEI: c = %.3lf, p = %.2e'%(c,p))
 
     return lowpass_b, peak_data, fit_params, b_pulses_onset, b_pulses_success
 
 def plot_one_side(fig, grid, sim_type, tstart, tstop, t, b, e, b_pulses, lowpass_b, peak_data, fit_params, b_pulses_onset, b_pulses_success):
 
     if peak_data != False:
-        peaks_start, peaks_end, peaks_duration_prev, peaks_duration_next, peaks_IEI_prev, peaks_IEI_next = peak_data
+        peaks_start, peaks_end, peaks_amp_prev, peaks_amp_next, peaks_duration_prev, peaks_duration_next, peaks_IEI_prev, peaks_IEI_next = peak_data
 
     ax_t_B = fig.add_subplot(grid[0, 0:4])
     ax_t_e = fig.add_subplot(grid[1:3, 0:4])
@@ -191,6 +221,7 @@ def plot_one_side(fig, grid, sim_type, tstart, tstop, t, b, e, b_pulses, lowpass
         ax_t_B.set_title(r'Evoked',loc='center',x=0.75,y=1.20,fontsize=pm.fonts)
 
     ax_t_B.plot(t, b, color='#3c3fef', lw=1.5)
+    # ax_t_B.plot(t, lowpass_b, color='Black', lw=1.5)
     ax_t_B.axhline(112,xmin=0.12,xmax=(0.12+1/6), linewidth=1, color='black')
     ax_t_B.text(tstart+(tstop-tstart)*0.08,120,'250 ms',fontsize=pm.fonts)
 
@@ -233,17 +264,16 @@ def plot_one_side(fig, grid, sim_type, tstart, tstop, t, b, e, b_pulses, lowpass
     if peak_data != False:
 
         if sim_type is 'spont':
-            ax_IEI_hist.set_title(r'\textbf{C1}',loc='left',x=-0.22,y=0.95,fontsize=pm.fonts)
-            ax_IEI_hist.hist(peaks_IEI_prev,bins=30,color='gray')
+            ax_IEI_hist.set_title(r'\textbf{C1}',loc='left',x=-0.22,y=0.80,fontsize=pm.fonts)
+            hist_n, _, _ = ax_IEI_hist.hist(peaks_IEI_prev,bins=30,color='gray')
         else:
-            ax_IEI_hist.set_title(r'\textbf{C2}',loc='left',x=-0.22,y=0.95,fontsize=pm.fonts)
-            ax_IEI_hist.hist(np.concatenate((peaks_IEI_prev,peaks_IEI_next)),bins=30,color='gray')
+            ax_IEI_hist.set_title(r'\textbf{C2}',loc='left',x=-0.22,y=0.80,fontsize=pm.fonts)
+            hist_n, _, _ = ax_IEI_hist.hist(np.concatenate((peaks_IEI_prev,peaks_IEI_next)),bins=30,color='gray')
 
-
-        ax_IEI_hist.set_title('IEI [s]', y=0.95, fontsize=pm.fonts)
-        ax_IEI_hist.set_xlim([0,3])
-        ax_IEI_hist.set_xticks([0,1,2])
-        ax_IEI_hist.set_xticklabels([0,1,2],fontsize=pm.fonts)
+        ax_IEI_hist.set_title('IEI [s]', y=0.80, fontsize=pm.fonts)
+        ax_IEI_hist.set_xlim([0,3.6])
+        ax_IEI_hist.set_xticks([0,1,2,3])
+        ax_IEI_hist.set_xticklabels([0,1,2,3],fontsize=pm.fonts)
         ax_IEI_hist.set_yticks([])
         ax_IEI_hist.set_yticklabels([])
 
@@ -252,28 +282,29 @@ def plot_one_side(fig, grid, sim_type, tstart, tstop, t, b, e, b_pulses, lowpass
         else:
             ax_prev.set_title(r'\textbf{D2}',loc='left',x=-0.15,y=0.95,fontsize=pm.fonts)
 
-        ax_prev.plot(peaks_IEI_prev, peaks_duration_prev, 'k.', ms=3)
+        ax_prev.plot(peaks_IEI_prev, peaks_amp_prev, 'k.', ms=3)
         ax_prev.axvline(np.min(peaks_IEI_prev), linewidth=1, color='k', linestyle='--')
         if fit_params.all != 0:
             x_array = np.arange(np.min(peaks_IEI_prev),np.max(peaks_IEI_prev),0.01)
             ax_prev.plot(x_array,fit_func(x_array,*fit_params),color='red', lw=1.5)
         ax_prev.set_xlabel('Previous IEI [s]',fontsize=pm.fonts)
-        if sim_type is 'spont': ax_prev.set_ylabel('FWHM [ms]',fontsize=pm.fonts)
-        ax_prev.set_ylim([30,105])
-        ax_prev.set_yticks([45,90])
-        ax_prev.set_yticklabels([45,90],fontsize=pm.fonts)
-        ax_prev.set_xlim([0,3])
-        ax_prev.set_xticks([0,1,2])
-        ax_prev.set_xticklabels([0,1,2],fontsize=pm.fonts)
+        # if sim_type is 'spont': ax_prev.set_ylabel('FWHM [ms]',fontsize=pm.fonts)
+        if sim_type is 'spont': ax_prev.set_ylabel('Amplitude [1/s]',fontsize=pm.fonts)
+        ax_prev.set_ylim([55,105])
+        ax_prev.set_yticks([75,100])
+        ax_prev.set_yticklabels([75,100],fontsize=pm.fonts)
+        ax_prev.set_xlim([0,3.6])
+        ax_prev.set_xticks([0,1,2,3])
+        ax_prev.set_xticklabels([0,1,2,3],fontsize=pm.fonts)
 
-        ax_next.plot(peaks_IEI_next, peaks_duration_next, 'k.', ms=3)
+        ax_next.plot(peaks_IEI_next, peaks_amp_next, 'k.', ms=3)
         ax_next.set_xlabel('Next IEI [s]',fontsize=pm.fonts)
-        ax_next.set_ylim([30,105])
-        ax_next.set_yticks([45,90])
-        ax_next.set_yticklabels([45,90],fontsize=pm.fonts)
-        ax_next.set_xlim([0,3])
-        ax_next.set_xticks([0,1,2])
-        ax_next.set_xticklabels([0,1,2],fontsize=pm.fonts)
+        ax_next.set_ylim([55,105])
+        ax_next.set_yticks([75,100])
+        ax_next.set_yticklabels([75,100],fontsize=pm.fonts)
+        ax_next.set_xlim([0,3.6])
+        ax_next.set_xticks([0,1,2,3])
+        ax_next.set_xticklabels([0,1,2,3],fontsize=pm.fonts)
 
 def plot_fig_12():
     print('============Spontaneous Events============')
@@ -301,8 +332,8 @@ def plot_fig_12():
 
     fig = plt.figure(figsize=(fig_width,fig_height))
     gs = gridspec.GridSpec(8, 13, width_ratios=[1,1,1,1,1,1,0.2,1,1,1,1,1,1])
-    gs_spont = gridspec.GridSpecFromSubplotSpec(5, 6, subplot_spec=gs[:, 0:6], height_ratios=[1.,0.45,0.3,0.25,1.])
-    gs_evoke = gridspec.GridSpecFromSubplotSpec(5, 6, subplot_spec=gs[:, 7:13], height_ratios=[1.,0.45,0.3,0.25,1.])
+    gs_spont = gridspec.GridSpecFromSubplotSpec(5, 6, subplot_spec=gs[:, 0:6], height_ratios=[1.,0.35,0.45,0.25,1.])
+    gs_evoke = gridspec.GridSpecFromSubplotSpec(5, 6, subplot_spec=gs[:, 7:13], height_ratios=[1.,0.35,0.45,0.25,1.])
 
     t_plot_start = 195.4*1e3
     t_plot_stop = 196.9*1e3
@@ -312,6 +343,6 @@ def plot_fig_12():
     plot_one_side(fig, gs_evoke, 'evoke', t_plot_start, t_plot_stop,\
                     t_evoke, b_evoke, e_evoke, b_pulses_evoke, lowpass_b_evoke, peak_data_evoke, fit_data_evoke, b_pulses_onset, b_pulses_success)
 
-    plt.subplots_adjust(wspace=1.5, hspace=0.25)
+    plt.subplots_adjust(wspace=2.0, hspace=0.25)
 
     fig.savefig('results/fig_rate_model_noise.eps', bbox_inches='tight', dpi=800)
